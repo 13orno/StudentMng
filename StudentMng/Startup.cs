fea78655d62bb9d8fa61ded28cc13724bdbd83cd
Md.Munaour Mehtab <engmunacse@gmail.com>
2016-08-20 23:41:38 +0600

    Add project files.

---
+++ StudentMng/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentMng.Startup))]
+namespace StudentMng
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
